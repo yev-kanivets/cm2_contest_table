92b0d130ee24f1afc6c07e4833a6b3ec4096e3a1
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2017-11-07 14:48:09 +0200

    [1h]. Pass students mock data to EJS template.

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  var div1Students = [{fullname: 'Коваленко Юрий', acmpId: '12345', dateTime: '02.11.2017 13:07:32', startRating: 1000, currentRating: 2000, bonuses: 0, contestRating: 1000},
+  {fullname: 'Коваленко Юрий', acmpId: '12345', dateTime: '02.11.2017 13:07:32', startRating: 1000, currentRating: 2000, bonuses: 0, contestRating: 1000}]
+  var div2Students = [{fullname: 'Коваленко Алексей', acmpId: '12345', dateTime: '02.11.2017 13:07:32', startRating: 1000, currentRating: 2000, bonuses: 0, contestRating: 1000}]
+  response.render('pages/index', {div1Students: div1Students, div2Students: div2Students});
 });
 
 app.listen(app.get('port'), function() {
