7c72bf8f47165cdc8b1461740eba1e07ead9b15b
Evgenii Kanivets <evgenii.kanivets@gmail.com>
2017-11-14 12:05:05 +0200

    [30m]. Add favicon. Change title.

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var favicon = require('serve-favicon');
+var path = require('path');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicons', 'favicon.ico')));
+
 var admin = require("firebase-admin");
 
 // Fetch the service account key JSON file contents
